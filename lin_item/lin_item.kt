90181c6d18a3ffae302bda11a4efad1b9020b35c
CHANG-HSIU-JU <80446776+CHANG-HSIU-JU@users.noreply.github.com>
2021-05-27 18:45:37 +0800

    Create lin_item.kt

---
+++ lin_item/lin_item.kt
@@ -0,0 +1,12 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lin_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lin_item)
+        title = "菜系"
+    }
+}
