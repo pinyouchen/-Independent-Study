88a2b7635f8c56b2d695f765eefd03967115f8f7
10936020 <80805514+10936020@users.noreply.github.com>
2021-09-29 17:32:36 +0800

    Add files via upload

--- insertRecipe.php
+++ insertRecipe.php
@@ -1,35 +1,47 @@
 <?php
-$serverName="localhost";
-$dbName="id17301288_employee_management_system";
-$userName="id17301288_louis321321";
-$password="J(*h>m>d)J^=8/UP";
+    $serverName="localhost";
+    $dbName="id17301288_employee_management_system";
+    $userName="id17301288_louis321321";
+    $password="J(*h>m>d)J^=8/UP";
+    
+    $conn = mysqli_connect($serverName,$userName,$password,$dbName);
+    
+    $user = $_POST["userid"];
+    $category = $_POST["category_id"];
+    $cooking = $_POST["cooking_id"];
+    $name = $_POST["recipe_name"];
+    $quantity = $_POST["quantity"];
+    $reTime = $_POST["release_time"];
+    $vegetarian = $_POST["vegetarian"];
+    $degree = $_POST["degree"];
+    $intro = $_POST["introduction"];
+    $cookTime = $_POST["cooking_time"];
+    $like = $_POST["likes"];
+    $privacy = $_POST["privacy"];
+    $inter = $_POST["international"];
 
-$conn = mysqli_connect($serverName,$userName,$password,$dbName);
-
-$user = $_POST["user_id"];
-$category = $_POST["category_id"];
-$cooking = $_POST["cooking_id"];
-$name = $_POST["recipe_name"];
-$quantity = $_POST["quantity"];
-$reTime = $_POST["release_time"];
-$vegetarian = $_POST["vegetarian"];
-$intro = $_POST["introduction"];
-$cookTime = $_POST["cooking_time"];
-$privacy = $_POST["privacy"];
-
-
+    $target_dir = "recipe_image/";
+	$image = $_POST["recipe_picture"];
+	$imageStore = rand()."_".time().".jpg";
+	$target_dir = $target_dir."/".$imageStore;
+	$uri="https://louis32132118.000webhostapp.com/recipe_image/".$imageStore;
+	file_put_contents($target_dir, base64_decode($image));
+         
+    $sql = "INSERT INTO recipe(userid,category_id,cooking_id,recipe_name,quantity,release_time,international,vegetarian,degree,introduction,cooking_time,likes,privacy) VALUES ('$user','$category','$cooking','$name','$quantity','$reTime','$inter','$vegetarian','$degree','$intro','$cookTime','$like','$privacy')";
+    
+    $result = $conn->query($sql);
      
-$sql = "INSERT INTO recipe
-        (user_id,category_id,cooking_id,recipe_name,quantity,release_time,vegetarian,degree,introduction,cooking_time,likes,privacy)
-        VALUES ('$user','$category','$cooking','$name','$quantity','$reTime','$vegetarian','0','$intro','$cookTime','0','$privacy')";
-     
-$result = mysqli_query($conn,$sql);
-     
-if($result){
-    echo "Data Inserted";
-}
-else{
-    echo "Failed";
-}
-mysqli_close($conn);
+    if($result){
+        
+        $recipe_id=$conn->insert_id;
+        $select= "INSERT into recipe_picture(recipe_id,recipe_picture) VALUES ('$recipe_id','$uri')";
+        $result = $conn->query($select);
+        echo "Data Inserted";
+        // header('Content-type: application/json');
+        echo json_encode($recipe_id);
+    }
+    else{
+        echo "Failed".mysqli_error($conn);
+    }
+    mysqli_close($conn);
 ?>
