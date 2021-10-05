eb3b9f3fdaa083b425c463b905b1247afe1a9679
10936020 <80805514+10936020@users.noreply.github.com>
2021-10-05 21:01:55 +0800

    Add files via upload

---
+++ insertList.php
@@ -0,0 +1,44 @@
+<?php
+    $serverName="localhost";
+    $dbName="id17301288_employee_management_system";
+    $userName="id17301288_louis321321";
+    $password="J(*h>m>d)J^=8/UP";
+    
+    $conn = mysqli_connect($serverName,$userName,$password,$dbName);
+
+    $recipeID=$_POST["recipeID"];
+    $quan=$_POST["quantity"];
+    $ingre=$_POST["ingredient"];
+
+    $sql = "SELECT * FROM ingredient WHERE Ingre='$ingre'";
+    $in_ingre = "INSERT INTO ingredient(Ingre) VALUES ('$ingre')";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_assoc($result);
+    if (!mysql_num_rows($result)) 
+    { 
+        $resultInsert=$conn->query($in_ingre);
+        if($resultInsert){
+            $ingre_id=$conn->insert_id;
+            $select="INSERT INTO recipe_ingredient(ingredient_id,recipe_id,quantity) VALUES('$ingre_id','$recipeID','$quan')";
+            $resultInsert=$conn->query($select);
+            echo "list Inserted";
+        }
+        else{
+            echo "Failed".mysqli_error($conn);
+        }
+    } 
+    else 
+    { 
+        $ingre_id=$row['id'];
+        $resultInsert=$conn->query($in_ingre);
+        if($resultInsert){
+            $select="INSERT INTO recipe_ingredient(ingredient_id,recipe_id,quantity) VALUES('$ingre_id','$recipeID','$quan')";
+            $resultInsert=$conn->query($select);
+            echo "list Inserted";
+        }
+        else{
+            echo "Failed".mysqli_error($conn);
+        }
+    }
+    mysqli_close($conn); 
+?>
