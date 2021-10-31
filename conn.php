9a817993850a6d746c2d69794c8b22b77d866625
10936020 <80805514+10936020@users.noreply.github.com>
2021-11-01 00:25:32 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+$serverName="localhost";
+$dbName="id17301288_employee_management_system";
+$userName="id17301288_louis321321";
+$password="Louis037662103";
+
+$conn = mysqli_connect($serverName,$userName,$password,$dbName) or die('Unable to connect');
+?>
